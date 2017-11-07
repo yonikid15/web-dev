3807fe3dec96857d80ab0a12e15bf624e164cd48
Yonathan <yoni.kid15@gmail.com>
2017-11-07 11:37:09 -0500

    Configured Gulp error handling for PostCSS changes, added Mixins dependency

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,10 +3,21 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, nested, cssvars, autoprefixer]))
+    .on('error', function(errorInfo) {
+        console.log(errorInfo.toString());
+        this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });
+
+/* .on('error', function() {...}) 
+
+    This 'on' function checks to see if this gulp task crashes due to any incorect syntax. It then makes sure it doesn't end the gulp task immediately, but instead continue running (this.emit('end')) by 'telling' gulp eveything ran smoothly
+    
+*/
